2bee4fe9e0d2658e3ad63766980c9a331d0b3aad
davidshalom-git <davidshalom2609@gmail.com>
2025-12-27 18:49:21 +0530

    GSC

--- client/src/Components/HeroSection.tsx
+++ client/src/Components/HeroSection.tsx
@@ -1,4 +1,5 @@
 import Navbar from './Navbar'
+import { motion } from 'framer-motion'
 
 const HeroSection = () => {
   return (
@@ -7,11 +8,11 @@ const HeroSection = () => {
       <div className='flex flex-col items-start justify-center px-6 md:px-16 lg:px-24 xl:px-32 text-white bg-gradient-to-r from-black/80 from-50% to-transparent to-50%, bg-[url("https://images.unsplash.com/photo-1506744038136-46273834b3fb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8Y29kZSUyMGJ1aWxkaW5nfGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60")] bg-cover bg-right min-h-screen bg-no-repeat'>
 
         <Navbar />
-        
-        <div className="flex flex-col items-center justify-center px-6 md:px-16 lg:px-24">
+
+        <motion.div initial={{ opacity: 0, y: 30 }} animate={{ opacity: 1, y: 0 }} transition={{ delay: 0.6, duration: 0.8 }} className="flex flex-col items-center justify-center px-6 md:px-16 lg:px-24">
           <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+Tamil:wght@400;500;600;700&display=swap" rel="stylesheet" />
 
-          <h1 className='text-5xl text-center mx-auto font-semibold mt-36 [font-family:"Noto_Sans_Tamil",sans-serif]'>
+          <h1 className='text-5xl text-center mx-auto font-bold mt-36 [font-family:"Noto_Sans_Tamil",sans-serif]'>
             பூரண சுவிசேஷ நல்ல மேய்ப்பன் தேவ சபை
           </h1>
 
@@ -22,11 +23,11 @@ const HeroSection = () => {
           <button className='mt-6 mx-auto bg-red-600 text-white px-10 py-2 rounded-full font-medium hover:bg-white hover:text-black transition-colors duration-300'>
             Live
           </button>
-        </div>
+        </motion.div>
       </div>
 
       <div>
-        <h1 className='text-4xl md:text-4xl text-center font-bold text-gray-800 mb-6 px-6 md:px-16 lg:px-24 xl:px-32 mt-10'>Promise Word</h1>
+        <h1 className='text-4xl md:text-4xl text-center font-bold text-gray-800 mb-6 px-6 md:px-16 lg:px-24 xl:px-32 mt-10 underline '>Promise Word</h1>
       </div>
     </>
   )
